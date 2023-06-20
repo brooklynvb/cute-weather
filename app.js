9e736584023a87ecc298b8ad1bad86ddf5e9c221
ANurfHerder <136250209+ANurfHerder@users.noreply.github.com>
2023-06-19 21:45:15 -0400

    Updated CSS and Show current temp

---
+++ app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "58ba5tfa5efecdeee334f3576a01o002";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Aiken&key=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
